91488b1ab7330916cbdefb673b1ceeefe46570cd
phyrowo <vicfermor22@gmail.com>
2024-11-04 11:42:46 +0100

    Commit 4/11

--- src/Tema2/Tema2Ejercicio30.java
+++ src/Tema2/Tema2Ejercicio30.java
@@ -1,19 +1,27 @@
-/*package Tema2;
+package Tema2;
 
 import java.util.Scanner;
 
 public class Tema2Ejercicio30 {
     public static void main(String[] args) {
-        double num1, num2, op;
+        double num1, num2, op, result;
         Scanner scan = new Scanner(System.in);
         System.out.println("Introduce el primero numero");
         num1 = scan.nextDouble();
+        System.out.println("Introduce el segundo numero");
+        num2 = scan.nextDouble();
         System.out.println("Introduce la operacion");
+        op = scan.nextInt();
 
-        switch (op) {
+        switch ((int) op) {
+            case '+': result = num1 + num2;
+            case '-': if (num1 > num2) {
+                        result = num1 - num2; }
+                        else { result = num2 - num1;}
+            case '*': result = num1 * num2;
+            }
 
         }
 
     }
 }
-*/
