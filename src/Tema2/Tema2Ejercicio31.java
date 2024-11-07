9b9b5a22ea6859f49bd462e6fda491d1a7043aff
VicentFer01 <vicfermor22@gmail.com>
2024-11-07 18:27:01 +0100

    Commit

--- src/Tema2/Tema2Ejercicio31.java
+++ src/Tema2/Tema2Ejercicio31.java
@@ -1,4 +1,23 @@
 package Tema2;
 
+import java.util.Scanner;
+
 public class Tema2Ejercicio31 {
-}
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num;
+        boolean esprimo;
+        System.out.println("Introduce un numero entero");
+        num = scan.nextInt();
+
+        if (num > 1) {
+            esprimo = false;
+        }
+        else if (num == 2 || num == 3) {
+            esprimo = true;
+        } else if (num % 2 == 0) {
+            
+        }
+
+
+    }
