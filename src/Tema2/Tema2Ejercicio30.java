5c91fa46a6d10f1d3701861cfc938945311c96cf
VicentFer01 <vicfermor22@gmail.com>
2024-11-07 17:58:11 +0100

    Commit

--- src/Tema2/Tema2Ejercicio30.java
+++ src/Tema2/Tema2Ejercicio30.java
@@ -6,39 +6,55 @@ public class Tema2Ejercicio30 {
     public static void main(String[] args) {
         double num1, num2, result = 0;
         char op;
+        String seguir = "";
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Introduce el primero numero");
-        num1 = scan.nextDouble();
+        do {
+            System.out.println("Introduce el primero numero");
+            num1 = scan.nextDouble();
 
-        System.out.println("Introduce el segundo numero");
-        num2 = scan.nextDouble();
+            System.out.println("Introduce el segundo numero");
+            num2 = scan.nextDouble();
 
-        System.out.println("Introduce la operacion (+, -, *, /):");
-        op = scan.next().charAt(0); // Read operation as a character
+            System.out.println("Introduce la operacion (+, -, *, /):");
+            op = scan.next().charAt(0);
 
-        switch (op) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = (num1 - num2);
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    System.out.println("Error");
-                }
-                break;
-            default:
-                System.out.println("Operacion no valida");
-                return;
-        }
+            switch (op) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0) {
+                        result = num1 / num2;
+                    } else {
+                        System.out.println("Error: División por cero.");
+                        continue;  // Skip to next iteration if division by zero
+                    }
+                    break;
+                default:
+                    System.out.println("Operacion no valida");
+                    continue;  // Skip to next iteration if invalid operation
+            }
 
-        System.out.println("El resultado es " + result);
+            System.out.println("El resultado es " + result);
+
+            System.out.println("¿Quieres hacer otra operación? (s para sí, cualquier otra tecla para no):");
+            seguir = scan.next();
+
+        } while (seguir.equalsIgnoreCase("s"));
+
+        System.out.println("Programa terminado.");
+        scan.close();
     }
 }
+
+
+
+
+
