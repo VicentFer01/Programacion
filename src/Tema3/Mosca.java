b7e8632ed21b4e15c72f6c0684791a65e9fa4ee6
VicentFer01 <vicfermor22@gmail.com>
2025-01-09 16:17:44 +0100

    fdsfdsfds

---
+++ src/Tema3/Mosca.java
@@ -0,0 +1,44 @@
+package Tema3;
+
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class Mosca {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+        int moscaPos = random.nextInt(15);
+        boolean moscaAtrapada = false;
+
+
+
+        int[] array = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+
+
+        do {
+            System.out.println("Introduce una posicion (1-15)");
+            int pos = scan.nextInt();
+
+        if (pos == moscaPos) {
+            System.out.println("¡Has atrapado a la mosca!");
+            moscaAtrapada = true;
+        } else if (pos == moscaPos + 1 || pos == moscaPos - 1) {
+            System.out.println("Casi la atrapas, la mosca se ha movido.");
+            moscaPos = random.nextInt(15);
+
+        } else {
+            System.out.println("No atrapaste a la mosca. Sigue intentando.");
+        }
+        } while (moscaAtrapada == false);
+
+
+
+
+
+
+    }
+
+
+}
---
+++ src/Tema3/Wifi.java
@@ -0,0 +1,18 @@
+package Tema3;
+
+import java.util.Scanner;
+
+public class Wifi {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Introduce el numero de la  habitación");
+        int hab = scan.nextInt();
+
+        System.out.print("Introduce la potencia de señal");
+        int senyal = scan.nextInt();
+
+        int[] array = new int[20];
+    }
+}
