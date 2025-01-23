af79a331dd0c9a9ae2ac5eb956b30f028619b5a6
vicen <vicfermor22@gmail.com>
2025-01-23 19:51:15 +0100

    Commit

--- src/Tema3/String/Strings.java
+++ src/Tema3/String/Strings.java
@@ -1,11 +1,12 @@
 package Tema3.String;
 
+import static Tema3.String.MyString.contarVocales;
 import static Tema3.String.MyString.invertirMayusString;
 
 public class Strings {
     public static void main(String[] args) {
-        String string = "Hola odio esta asignatura quiero irme a ASIR";
-        String str = invertirMayusString(string);
-        System.out.println(str);
+        String string = "Hola";
+        int vocales = contarVocales(string);
+        System.out.println("Hay " + vocales + " vocales");
     }
 }
