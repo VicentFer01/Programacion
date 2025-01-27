05cebd7b36d4a1c3296f59a5dad13dc27552dada
vicfermor3 <vicfermor22@gmail.com>
2025-01-27 12:21:35 +0100

    COmmit

--- src/Objetos/HeroMain.java
+++ src/Objetos/HeroMain.java
@@ -31,13 +31,13 @@ public class HeroMain {
 
             // Crear enemigos
             for (int i = 0; i < hordaNumber; i++) {
-                Hero enemy = new Hero(enemyName + " " + i);
+                Hero enemy = new Hero(enemyName + " " + (i + 1));
                 enemy.setHealth(heroe1.getMaxHealth() / hordaNumber);
                 enemy.setMaxHealth(heroe1.getMaxHealth() / hordaNumber);
                 enemy.setAttack(heroe1.getAttack() / hordaNumber);
                 enemy.setDefense(heroe1.getMaxDefense() / hordaNumber);
                 enemigos[i] = enemy;
-                System.out.println(enemy);
+                System.out.println((i + 1) + ". " + enemy);
             }
 
             // Batalla con los enemigos
@@ -45,40 +45,53 @@ public class HeroMain {
             while (hordaViva && heroe1.getHealth() > 0) {
                 hordaViva = false; // Suponemos que no quedan enemigos vivos
 
+                // Mostrar enemigos vivos
+                System.out.println("Enemigos disponibles para atacar:");
                 for (int i = 0; i < enemigos.length; i++) {
-                    Hero enemy = enemigos[i];
+                    if (enemigos[i].getHealth() > 0) {
+                        System.out.println((i + 1) + ". " + enemigos[i].getName() + " (Vida: " + enemigos[i].getHealth() + ")");
+                    }
+                }
 
-                    if (enemy.getHealth() > 0) { // Solo interactuar si el enemigo está vivo
-                        hordaViva = true; // Hay al menos un enemigo vivo
+                System.out.println("Selecciona el número del enemigo al que quieres atacar:");
+                int seleccion = scan.nextInt() - 1; // Convertir a índice (de 1-3 a 0-2)
 
-                        System.out.println();
-                        System.out.println("Escribe 1 para atacar a " + enemy.getName());
-                        int seleccion = scan.nextInt();
+                if (seleccion >= 0 && seleccion < enemigos.length && enemigos[seleccion].getHealth() > 0) {
+                    Hero enemy = enemigos[seleccion];
 
-                        if (seleccion == 1) {
-                            int heroDamage = heroe1.heroAttack(enemy); // Daño del héroe al enemigo
-                            System.out.println(heroe1.getName() + " ataca a " + enemy.getName() + " causando " + heroDamage + " de daño.");
-                            System.out.println(enemy.getName() + " tiene " + enemy.getHealth() + " puntos de vida");
+                    // Ataque del héroe
+                    int heroDamage = heroe1.heroAttack(enemy);
+                    System.out.println(heroe1.getName() + " ataca a " + enemy.getName() + " causando " + heroDamage + " de daño.");
+                    System.out.println(enemy.getName() + " tiene " + enemy.getHealth() + " puntos de vida.");
 
-                            if (enemy.getHealth() <= 0) { // Si el enemigo muere
-                                System.out.println(enemy.getName() + " ha sido derrotado.");
-                            } else { // Si el enemigo sigue vivo, contraataca
-                                int enemyDamage = enemy.enemyAttack(heroe1);
-                                System.out.println(enemy.getName() + " contraataca causando " + enemyDamage + " de daño.");
-                                System.out.println(heroe1.getName() + " tiene ahora " + heroe1.getHealth() + " puntos de vida");
+                    if (enemy.getHealth() <= 0) { // Si el enemigo muere
+                        System.out.println(enemy.getName() + " ha sido derrotado.");
+                    } else { // Si el enemigo sigue vivo, contraataca
+                        int enemyDamage = enemy.enemyAttack(heroe1);
+                        System.out.println(enemy.getName() + " contraataca causando " + enemyDamage + " de daño.");
+                        System.out.println(heroe1.getName() + " tiene ahora " + heroe1.getHealth() + " puntos de vida.");
 
-                                if (heroe1.getHealth() <= 0) { // Si el héroe muere
-                                    System.out.println(heroe1.getName() + " ha sido derrotado. Fin del juego.");
-                                    break;
-                                }
-                            }
+                        if (heroe1.getHealth() <= 0) { // Si el héroe muere
+                            System.out.println(heroe1.getName() + " ha sido derrotado. Fin del juego.");
+                            break;
                         }
+                    }
+                } else {
+                    System.out.println("Selección inválida. Por favor, elige un enemigo válido.");
+                }
+
+                for (Hero enemy : enemigos) {
+                    if (enemy.getHealth() > 0) {
+                        hordaViva = true;
+                        break;
                     }
                 }
             }
 
             if (!hordaViva) {
                 System.out.println("Has derrotado a todos los enemigos de la horda.");
+                heroe1.levelUP();
+                System.out.println("H");
             }
         }
     }
